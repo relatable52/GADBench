7cf7acc1e103cac999e9d5f67d09bcd109177f0a
relatable52 <duongchinh29062003@gmail.com>
2024-06-16 05:40:57 +0700

    Fix dataset

--- dataset.py
+++ dataset.py
@@ -67,7 +67,7 @@ class Dataset:
             train_ratio, val_ratio = 0.5, 0.25
         if self.name in ['tsocial', 'tfinance', 'reddit', 'weibo']:
             train_ratio, val_ratio = 0.4, 0.2
-        if self.name in ['amazon', 'yelp', 'elliptic', 'dgraphfin']:  # official split
+        if self.name in ['amazon', 'yelp', 'elliptic', 'elliptic_lf', 'dgraphfin']:  # official split
             train_masks[:,:10] = self.graph.ndata['train_mask'].repeat(10,1).T
             val_masks[:,:10] = self.graph.ndata['val_mask'].repeat(10,1).T
             test_masks[:,:10] = self.graph.ndata['test_mask'].repeat(10,1).T
