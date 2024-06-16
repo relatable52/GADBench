ad36f75ca67a2d887eac25f4a5404cb4e1a6b771
relatable52 <duongchinh29062003@gmail.com>
2024-06-16 07:20:20 +0700

    save node embedding

--- save_ne.py
+++ save_ne.py
@@ -56,7 +56,7 @@ test_score = detector.train()
 gcn_model = detector.model
 graph = detector.source_graph
 h = gcn_model.get_hidden_state(graph)
-print(graph.nodes)
+print(graph.nodes())
 print(h, h.shape)
 print(graph.ndata['feature'])
 
